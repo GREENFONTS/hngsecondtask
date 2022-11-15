665a02efdd86cb24fbe8dcf511ec54af4a2f32f6
GREENFONTS <godwillonyewuchii@gmail.com>
2022-11-15 15:05:54 +0100

    feat: added lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,32 @@
 import { createBrowserRouter } from 'react-router-dom';
+import React, { Suspense } from 'react';
 import App from './App';
-import HomeComponent from './pages/home';
-import PlaceToStay from './pages/PlaceToStay';
+const HomePage = React.lazy(() => import('./pages/home'));
+const PlaceToStay = React.lazy(() => import('./pages/PlaceToStay'));
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [{
-      path: "/",
-      element: <HomeComponent />
-    }, 
-    {
-      path: "/placetostay",
-      element: <PlaceToStay />
-    }]
+    children: [
+      {
+        path: '/',
+        element: (
+          <Suspense fallback={<>Loading...</>}>
+            <HomePage />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/placetostay',
+        element: (
+          <Suspense fallback={<>Loading...</>}>
+            <PlaceToStay />
+          </Suspense>
+        ),
+      },
+    ],
   },
 ]);
-
 
 export default router;

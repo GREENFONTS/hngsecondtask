55511d066cd0a5df4a5a023e598152d2ebc59eae
GREENFONTS <godwillonyewuchii@gmail.com>
2022-11-15 14:17:05 +0100

    fix: Image size fix

--- src/components/HomeComponents/FirstSection.jsx
+++ src/components/HomeComponents/FirstSection.jsx
@@ -30,14 +30,20 @@ const FirstSection = () => {
         >
           <Heading
             fontSize={{
-              base:"30px",
+              base: '30px',
               sm: '25px',
               md: '30px',
               lg: '38px',
               xl: '48px',
               '2xl': '57px',
             }}
-            lineHeight={{base:"130%", sm: '110%', md: '120%', xl: '140%', '2xl': '130%' }}
+            lineHeight={{
+              base: '130%',
+              sm: '110%',
+              md: '120%',
+              xl: '140%',
+              '2xl': '130%',
+            }}
             color="#434343"
             fontWeight="400"
             letterSpacing="-0.02em"
@@ -55,7 +61,7 @@ const FirstSection = () => {
 
           <Flex
             mt={{
-              base:"20px",
+              base: '20px',
               sm: '25px',
               md: '27px',
               lg: '34.1px',
@@ -65,7 +71,7 @@ const FirstSection = () => {
           >
             <Text
               fontSize={{
-                base:"15px",
+                base: '15px',
                 sm: '11px',
                 md: '12.7px',
                 lg: '18px',
@@ -96,7 +102,7 @@ const FirstSection = () => {
               borderRadius="0px 8px 8px 0px"
               w={{ base: '35.6%' }}
               bg="linear-gradient(90deg, #A02279 11.45%, #A02279 11.46%)"
-              fontSize={{base:"14px", sm: '12px', md: '16px' }}
+              fontSize={{ base: '14px', sm: '12px', md: '16px' }}
               lineHeight="19px"
               textAlign="center"
               color="#FFFFFF"
@@ -109,33 +115,30 @@ const FirstSection = () => {
             </Button>
           </Flex>
         </Box>
-        <Flex w={{base:"100%", sm: '38.4%' }} mt={{base:"20px", sm:"0px"}} gap="5px" animation="bounceFromBottom 0.3s">
-          <VStack mt={{base:"60px", sm: '45px', md: '60px', lg: '71px', xl: '99px' }}>
-            <Image
-              src="assets/png/Home/image 4.png"
-              h={{ md: '120px', lg: '155px', xl: '176px' }}
-              w={{ md: '150px', lg: '200px', xl: '234px' }}
-            />
+        <Flex
+          w={{ base: '100%', sm: '38.4%' }}
+          mt={{ base: '20px', sm: '0px' }}
+          gap="5px"
+          animation="bounceFromBottom 0.3s"
+        >
+          <VStack
+            mt={{
+              base: '60px',
+              sm: '45px',
+              md: '60px',
+              lg: '71px',
+              xl: '99px',
+            }}
+          >
+            <Image src="assets/png/Home/image 4.png" />
 
-            <Image
-              src="assets/png/Home/image 6.png"
-              h={{ md: '120px', lg: '155px', xl: '176px' }}
-              w={{ md: '150px', lg: '200px', xl: '234px' }}
-            />
+            <Image src="assets/png/Home/image 6.png" />
           </VStack>
 
           <VStack>
-            <Image
-              src="assets/png/Home/image 3.png"
-              h={{ md: '120px', lg: '155px', xl: '176px' }}
-              w={{ md: '150px', lg: '200px', xl: '234px' }}
-            />
+            <Image src="assets/png/Home/image 3.png" />
 
-            <Image
-              src="assets/png/Home/image 5.png"
-              h={{ md: '120px', lg: '155px', xl: '176px' }}
-              w={{ md: '150px', lg: '200px', xl: '234px' }}
-            />
+            <Image src="assets/png/Home/image 5.png" />
           </VStack>
         </Flex>
       </Flex>
